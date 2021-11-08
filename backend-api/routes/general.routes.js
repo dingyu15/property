b1996b579b2578cab27de835f1fe1b7698a01503
Jane Lim <jane_limsj@hotmail.com>
2021-11-08 23:18:40 +0800

    update

--- backend-api/routes/general.routes.js
+++ backend-api/routes/general.routes.js
@@ -6,9 +6,8 @@ const {agentSignUp, agentLogin, findSelectedAgent, getAllAgents} = require('../c
 router.post('/agent/register', async (request,response) => await agentSignUp(request,response));
 router.post('/agent/login', async (request,response) => await agentLogin(request,response));
     //Change the request.params variable name ':agentId' if required
-router.get('/agent/:agentId', async (request,response) => await findSelectedAgent(request,response));
-router.get('/agent/allAgents', async (request,response) => await getAllAgents(request,response));
-
+router.get('/agent/:agentId', async (request, response) => await findSelectedAgent(request,response));
+router.get('/agent/allAgents', async (request, response) => await getAllAgents(request,response));
 
 const {customerSignUp, customerLogin} = require('../controllers/customer.controller');  
 router.post('/customer/register', async (request,response) => await customerSignUp(request,response));
